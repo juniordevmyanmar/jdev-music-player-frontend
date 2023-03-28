f425cc7d2c4bf5e2f88f1fb74d41a5b4aaf0dcd3
Kyaw wunna <66557969+kyawwunna356@users.noreply.github.com>
2023-03-28 20:32:13 +0630

    Project setup (#4)
    * Initialize project using Create React App
    
    * initial commit
    
    * added jdev templates

---
+++ src/contexts/GlobalStateProvider.tsx
@@ -0,0 +1,11 @@
+// implement global state here
+
+import React from 'react'
+
+function GlobalStateProvider() {
+  return (
+    <div>GlobalStateProvider</div>
+  )
+}
+
+export default GlobalStateProvider
